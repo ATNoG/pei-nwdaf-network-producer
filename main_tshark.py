43d04cda2c62fad34ede95323d8318b249470c4d
vcnt <thiago.vicente@ua.pt>
2026-02-26 23:39:01 +0000

    Add subscription logic to main_tshark

--- main_tshark.py
+++ main_tshark.py
@@ -1,12 +1,18 @@
 import argparse
+import uvicorn
+import threading
 import json
 import logging
 import os
 import sys
 import time
+from time import sleep
 
 import requests
 import yaml
+
+from src.router import ApiRouter
+from src.subscription_registry import SubscriptionRegistry
 
 logger = logging.getLogger("Packet capture producer")
 logging.basicConfig(level=logging.DEBUG)
@@ -36,28 +42,6 @@ def try_numeric(value: str) -> int | float | str:
     return value
 
 
-def send_batch(batch: list, api_url: str, producer_id: str, event_id: str):
-
-    payload = {
-        "analyticsData": [
-            {
-                "analyticsMetadata": record,
-                "timestamp": record.pop("timestamp", int(time.time())),
-            }
-            for record in batch
-        ],
-        "producerId": producer_id,
-        "eventId": event_id,
-    }
-
-    try:
-        response = requests.post(api_url, json=payload, timeout=5)
-        response.raise_for_status()
-        print(f"Sent batch of {len(batch)} lines successfully")
-    except requests.RequestException as e:
-        print(f"Error sending batch to API: {e}")
-
-
 def flatten(layers: dict, allowed_fields: set | None = None) -> dict:
 
     def _flatten_intern(obj: dict, result: dict | None = None) -> dict:
@@ -76,10 +60,58 @@ def flatten(layers: dict, allowed_fields: set | None = None) -> dict:
     return _flatten_intern(layers)
 
 
-def main(cell_index: int, api_url: str, event_id: str, interval: int):
+def send_batch(
+    batch: list,
+    subscription_registry: SubscriptionRegistry,
+    producer_id: str,
+    event_id: str,
+):
+
+    if not batch:
+        return
+
+    payload = {
+        "analyticsData": [
+            {
+                "analyticsMetadata": record,
+                "timestamp": record.pop("timestamp", int(time.time())),
+            }
+            for record in batch
+        ],
+        "producerId": producer_id,
+        "eventId": event_id,
+    }
+
+    for subscription_id in subscription_registry.all_subscribers():
+        payload["subscription_id"] = subscription_id
+        try:
+            response = requests.post(
+                subscription_registry.get_url(subscription_id), json=payload, timeout=5
+            )
+            response.raise_for_status()
+            print(f"Sent batch of {len(payload['analyticsData'])} lines successfully")
+        except requests.RequestException as e:
+            print(f"Error sending batch to API: {e}")
+            subscription_registry.record_failure(subscription_id)
+
+
+def main(
+    cell_index: int,
+    event_id: str,
+    interval: float,
+    send_after: float,
+    no_filter: bool,
+    port: int,
+):
+
+    subscription_registry = SubscriptionRegistry(max_failures=5)
+    api = ApiRouter(subscription_registry)
+
+    api_thread = threading.Thread(target=start_api, args=[api, port])
+    api_thread.start()
 
     batch = []
-    last_send = time.time()
+    last_send: int = int(time.time())
 
     # Read tshark JSON lines from stdin
     for line in sys.stdin:
@@ -90,7 +122,6 @@ def main(cell_index: int, api_url: str, event_id: str, interval: int):
         data: dict
         try:
             data = json.loads(line)
-
         except json.JSONDecodeError:
             logger.warning(f"Failled to convert line to json: {line}")
             continue
@@ -103,7 +134,7 @@ def main(cell_index: int, api_url: str, event_id: str, interval: int):
             exit(1)
 
         allowed_fields = None
-        if not args.no_filter:
+        if not no_filter:
             with open(FIELDS_FILE) as f:
                 allowed_fields = set(yaml.safe_load(f))
 
@@ -112,16 +143,21 @@ def main(cell_index: int, api_url: str, event_id: str, interval: int):
         record["timestamp"] = int(data.get("timestamp", time.time() * 1000)) / 1000
         batch.append(record)
 
-        # Send batch on interval
-        if time.time() - last_send >= interval:
-            if batch:
-                send_batch(batch, api_url, "packet-capture", event_id)
-                batch = []
-            last_send = time.time()
+        if time.time() - last_send >= send_after:
+            send_batch(batch, subscription_registry, PRODUCER_ID, event_id)
+            batch = []
+            last_send = int(time.time())
+
+        sleep(interval)
 
     # Send remaining
     if batch:
-        send_batch(batch, api_url, "packet-capture", event_id)
+        send_batch(batch, subscription_registry, PRODUCER_ID, event_id)
+
+
+def start_api(api: ApiRouter, port: int):
+    api.create_routes()
+    uvicorn.run(api.app, host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
@@ -143,24 +179,26 @@ if __name__ == "__main__":
         help="Cell index to be added on POST ( for compability with the existing backend) ",
     )
     parser.add_argument(
-        "-a",
-        "--api",
-        type=str,
-        default="https://webhook.site/d2c1b083-911c-435e-89f4-48467917c345",
-        help="API URL to send data to",
-    )
-    parser.add_argument(
         "--no-filter",
         action="store_true",
         default=False,
         help="Disable packet filter",
     )
     parser.add_argument(
-        "-s", "--send", type=str, default=5, help="Interval between batch sends"
+        "-s", "--send", type=float, default=5, help="Interval between batch sends"
     )
     parser.add_argument(
         "-y", "--type", type=str, default="network_trafic", help="Data type"
     )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=int(os.getenv("PORT", 8000)),
+        help="Port of subscription api",
+    )
 
     args = parser.parse_args()
-    main(args.cell_index, args.api, args.type, args.interval)
+    main(
+        args.cell_index, args.type, args.interval, args.send, args.no_filter, args.port
+    )
