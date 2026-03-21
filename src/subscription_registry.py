366343cd80123b9dfb6f3fc2ae6879ddbfc3c61a
JPSP9547 <joaopp@ua.pt>
2026-03-21 16:32:39 +0000

    add more room for some timeouts

--- src/subscription_registry.py
+++ src/subscription_registry.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class SubscriptionRegistry:
-    def __init__(self, max_failures: int = 10):
+    def __init__(self, max_failures: int = 30):
         self.subscribers: Dict[str, str] = {}
         self.subs_failures : Dict[str, int] = {}
         self.max_failures = max_failures
